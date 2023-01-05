cb869a0422b36e1092c90743627d45b6839785c3
Nirmalraj2530 <111743054+Nirmalraj2530@users.noreply.github.com>
2023-01-05 18:46:07 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import Formjson from './Components/formjson';
+// import './App.css';  
+
+function App() {
+  return (
+    <div className="App">
+      <Formjson></Formjson>
+    </div>
+  );
+}
+
+export default App;
